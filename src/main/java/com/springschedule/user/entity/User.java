aa4445410bda0e3ca35c3abc6a3f739a578b86d8
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-10 19:50:51 +0900

    feat: 유저 엔티티 및 레포지토리 추가

---
+++ src/main/java/com/springschedule/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.springschedule.user.entity;
+
+import com.springschedule.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 8)
+    private String userName;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/springschedule/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springschedule.user.repository;
+
+import com.springschedule.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
