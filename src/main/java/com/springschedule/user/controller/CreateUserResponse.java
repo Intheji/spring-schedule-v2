114fc67a80cc3b1a6583628604169af17a11e1f9
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-10 19:57:54 +0900

    feat: 유저 생성 dto 추가

---
+++ src/main/java/com/springschedule/user/controller/CreateUserResponse.java
@@ -0,0 +1,22 @@
+package com.springschedule.user.controller;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CreateUserResponse {
+    private final Long id;
+    private final String UserName;
+    private final String email;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    public CreateUserResponse(Long id, String userName, String email, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        UserName = userName;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+}
