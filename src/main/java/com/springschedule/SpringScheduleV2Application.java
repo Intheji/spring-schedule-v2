909f25cd3028b28988c22e8ad5e707048a111e9d
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-10 10:48:32 +0900

    chore: 프로젝트 초기 세팅 및 DB 환경변수 구성

---
+++ src/main/java/com/springschedule/SpringScheduleV2Application.java
@@ -0,0 +1,13 @@
+package com.springschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springschedule/SpringScheduleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
