99010ae79ddef83bfd1d769ec5cb188ea6b21c2d
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-10 11:33:01 +0900

    feat: 일정 조회용 Repository 메서드 추가

---
+++ src/main/java/com/springschedule/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.springschedule.schedule.repository;
+
+import com.springschedule.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    // 쿼리 메서드로 전체 조회 - 수정일 내림차순
+    List<Schedule> findAllByOrderByModifiedAtDesc();
+
+    // 쿼리 메서드로 작성자명 조회 - 수정일 내림차순
+    List<Schedule> findByAuthorNameOrderByModifiedAtDesc(String authorName);
+}
