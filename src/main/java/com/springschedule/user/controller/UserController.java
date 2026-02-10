782d6b0bfc885be9c6e82ec382200d436a5dd918
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-10 20:25:03 +0900

    feat: 유저 조회 컨트롤러 추가

--- src/main/java/com/springschedule/user/controller/UserController.java
+++ src/main/java/com/springschedule/user/controller/UserController.java
@@ -2,15 +2,15 @@ package com.springschedule.user.controller;
 
 import com.springschedule.user.dto.CreateUserRequest;
 import com.springschedule.user.dto.CreateUserResponse;
+import com.springschedule.user.dto.GetUserResponse;
 import com.springschedule.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,17 @@ public class UserController {
             @Valid @RequestBody CreateUserRequest request
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetUserResponse>> getUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<GetUserResponse> getUser(
+            @PathVariable Long userId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findOne(userId));
     }
 }
