e48c939aee0114b1a6146e5ff73b753c39211c91
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-12 18:07:05 +0900

    refactor: Schedule/User/Comment 응답 dto record로 바꿈

--- src/main/java/com/springschedule/schedule/dto/ScheduleResponse.java
+++ src/main/java/com/springschedule/schedule/dto/ScheduleResponse.java
@@ -1,27 +1,13 @@
 package com.springschedule.schedule.dto;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-public class ScheduleResponse {
-
-    private final Long id;
-    private final String title;
-    private final String content;
-    private final String userName;
-    private final Long commentCount;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
-
-    public ScheduleResponse(Long id, String title, String content, String userName, Long commentCount, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.userName = userName;
-        this.commentCount = commentCount;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
-}
+public record ScheduleResponse(
+        Long id,
+        String title,
+        String content,
+        String userName,
+        Long commentCount,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
+) {}
--- src/main/java/com/springschedule/user/dto/UserResponse.java
+++ src/main/java/com/springschedule/user/dto/UserResponse.java
@@ -1,23 +1,12 @@
 package com.springschedule.user.dto;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-public class UserResponse {
-    private final Long id;
-    private final String userName;
-    private final String email;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
 
-    public UserResponse(Long id, String userName, String email,
-                        LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.id = id;
-        this.userName = userName;
-        this.email = email;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
-}
+public record UserResponse (
+    Long id,
+    String userName,
+    String email,
+    LocalDateTime createdAt,
+    LocalDateTime modifiedAt
+) {}
