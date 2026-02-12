94b0e71024259b82599dedbfcf4046382a0e34de
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-13 02:35:37 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/com/springschedule/comment/repository/CommentRepository.java
+++ src/main/java/com/springschedule/comment/repository/CommentRepository.java
@@ -10,9 +10,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 일정의 댓글을 생성일 오름차순으로 조회
     List<Comment> findAllBySchedule_IdOrderByCreatedAtAsc(Long scheduleId);
 
-    // 댓글 일괄 삭제 쿼리 메서드
-    void deleteAllBySchedule_Id(Long scheduleId);
-
     // 일정 안에 댓글 단건 조회 쿼리메서드
     Optional<Comment> findByIdAndSchedule_Id(Long id, Long scheduleId);
 
